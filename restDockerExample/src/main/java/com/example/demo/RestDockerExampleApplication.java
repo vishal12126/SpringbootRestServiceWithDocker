19569a9a9e39215a5826373982f731772fe3f726
vishal12126 <vishal.ss.singh1021@gmail.com>
2019-05-28 14:39:34 +0530

    Project files

---
+++ restDockerExample/src/main/java/com/example/demo/RestDockerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestDockerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestDockerExampleApplication.class, args);
+	}
+
+}
---
+++ restDockerExample/src/main/java/com/example/demo/Welcome.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class Welcome {
+public Welcome(String message) {
+super();
+this.message = message;
+}
+private final String message;
+public String getMessage() {
+return message;
+}
+}
